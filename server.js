7e5ab71f809bba7a6eadd60493b53f2688b807f6
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-01-30 15:10:18 +0200

    working on delete row

--- server.js
+++ server.js
@@ -54,13 +54,24 @@ MongoClient.connect(connectionString)
     })
   
   // trying to delete row from database mongoDB
-    // app.delete('/quotes', (req, res) => {
-    //   quotesCollection
-    //   .deleteOne(query, options)
-    //   .then(result => {
-    //     res.json(`Deleted Darth Vader's quote`)
-    //   })
-    //   .catch(error => console.error(error))    })
+
+//   const deleteButton = document.querySelector('#delete-button')
+
+//   deleteButton.addEventListener('click', _ => {
+//     fetch('/quotes', {
+//       method: 'delete',
+//       headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//       name: 'Darth Vader'
+//     })
+//   })
+//   .then(res => {
+//     if (res.ok) return res.json()
+//   })
+//   .then(data => {
+//     window.location.reload()
+//   })
+// })
 
 
 
